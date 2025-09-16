52651c13ac7b35c059ccca56a9c15d8fa28b3f25
Erik Medeiros <erik.medeiros.701@ufrn.edu.br>
2025-09-16 13:58:16 -0300

    Add security configuration and user authentication filter

---
+++ src/main/java/br/erik/apiauthmodel/security/config/SecurityConfiguration.java
@@ -0,0 +1,70 @@
+package br.erik.apiauthmodel.security.config;
+
+import br.erik.apiauthmodel.security.auth.UserAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    private UserAuthenticationFilter userAuthenticationFilter;
+
+    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
+            "/users/login", //url que usaremos para fazer login
+            "/users" //url que usaremos para criar um usuário
+    };
+
+    // Endpoints que requerem autenticação para serem acessados
+    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {
+            "/users/test"
+    };
+
+    // Endpoints que só podem ser acessador por usuários com permissão de cliente
+    public static final String [] ENDPOINTS_CUSTOMER = {
+            "/users/test/customer"
+    };
+
+    // Endpoints que só podem ser acessador por usuários com permissão de administrador
+    public static final String [] ENDPOINTS_ADMIN = {
+            "/users/test/administrator"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // Desativa a proteção contra CSRF
+            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Configura a política de criação de sessão como stateless
+            .authorizeHttpRequests(auth -> auth // Habilita a autorização para as requisições HTTP
+                    .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
+                    .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_REQUIRED).authenticated()
+                    .requestMatchers(ENDPOINTS_ADMIN).hasRole("ADMINISTRATOR") // Repare que não é necessário colocar "ROLE" antes do nome, como fizemos na definição das roles
+                    .requestMatchers(ENDPOINTS_CUSTOMER).hasRole("CUSTOMER")
+                    .anyRequest().denyAll()
+            )
+           // Adiciona o filtro de autenticação de usuário que criamos, antes do filtro de segurança padrão do Spring Security
+            .addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
